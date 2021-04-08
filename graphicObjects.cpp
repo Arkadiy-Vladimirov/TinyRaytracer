154cff090bd4b1bd48aea30859d526c9d428b5f6
Arkadiy-Vladimirov <archibaldvladimirov@gmail.com>
2021-04-08 21:04:07 +0300

    tiny classes' descrtiptions changes

--- graphicObjects.cpp
+++ graphicObjects.cpp
@@ -1,6 +1,8 @@
 #include "graphicObjects.hpp"
 #include "Image.h"
 
-Camera::Camera(int resX, int resY, int a_channels, Point3 foc_pos) : matrix(resX, resY, a_channels){
+Camera::Camera(Vec3 foc_pos, Vec3 v_dir, Vec2 c_fov, Vec2 resolution, int a_channels) : matrix(resolution.x, resolution.y, a_channels){
     focus = foc_pos;
+    view_direction = v_dir;
+    fov = c_fov;
 };
--- graphicObjects.hpp
+++ graphicObjects.hpp
@@ -1,18 +1,24 @@
 #include "Image.h"
 
-struct Point3 {
-    double x, y, z;
+struct Vec2 {
+    double x, y;
 
-    Point3() {x = y = z = 0;}
-    Point3(int x1) {x = x1; y = z = 0;} 
+    Vec2(double x1 = 0, double x2 = 0) {x = x1; y = x2;}
+};
+struct Vec3 : Vec2{
+    double z;
+
+    Vec3(double x1 = 0, double x2 = 0, double x3 = 0) : Vec2(x1, x2) {z = x3;} 
 };
 
-class Camera {
-   Point3 focus;
-   Image matrix;
+class Camera { //view direction?
+    Vec3 focus;
+    Vec3 view_direction;
+    Vec2 fov; //degrees
+    Image matrix;
 
 public:
-    Camera(int resX = 1024, int resY = 1024, int a_channels = 4, Point3 foc_pos = 0);
+    Camera(Vec3 foc_pos, Vec3 v_dir, Vec2 fov = Vec2(90,60), Vec2 resolution = Vec2(1024, 1024), int a_channels = 4);
 };
 
 class GraphObject {
