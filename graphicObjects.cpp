e5bb0b41bf9cd5f14ccb1c36c587dcb7f97e7c7c
Arkadiy-Vladimirov <archibaldvladimirov@gmail.com>
2021-04-20 00:26:24 +0300

    commit 20th april

--- graphicObjects.cpp
+++ graphicObjects.cpp
@@ -3,6 +3,8 @@
 
 #include "graphicObjects.hpp"
 
+float Camera::render_distance = 1000;
+
 Camera::Camera(Vec3f f_origin, Vec3f view_dir, double f_fov, Vec2un resolution, int a_channels, Vec2f f_mat_size) : cam_base(f_origin, view_dir), matrix(resolution.x, resolution.y, a_channels)  { //warning: channels!
     fov = f_fov;
     matrix_size = f_mat_size;
@@ -19,9 +21,9 @@ Camera::Camera(Vec3f f_origin, Vec3f view_dir, double f_fov, Vec2un resolution, 
 const Image& Camera::RenderImage(const Scene& scene) {
     for (int x = 0; x < matrix.Width(); ++x) {
     for (int y = 0; y < matrix.Height(); ++y) {
-        //Ray(orig,dir);
-        //Color col = Ray.Cast();    
-        //matrix.PutPixel(x,y,Pixel{col.r, col.g ,col.b, 255}); 
+        Ray ray(cam_base.orig, GetPixelLocation(x,y) - cam_base.orig);
+        Color col = ray.Cast(scene);
+        matrix.PutPixel(x, y, Pixel{col.r, col.g ,col.b, 255});
     }
     }
     return matrix;
@@ -34,10 +36,38 @@ Scene::~Scene() {
     delete[] data;
 };
 
-GraphObject*& Scene::operator[](unsigned idx) {
+GraphObject*& Scene::operator[](unsigned idx) const{
     if (idx < size)
         return data[idx];
     else throw "error: array index out of bounds";
+};
+
+
+Vec3f MonochromeSphere::CheckHit(const Ray& ray) const {
+    return Vec3f();
+};
+
+Color MonochromeSphere::Hit(const Ray& ray) const {
+    return Color();
+};
+
+Color Ray::Cast(const Scene& scene) const {
+    Vec3f old_hit_point = Camera::GetRenderDistance() * direction; //inaccurate
+    Color resCol; int hit_obj_idx = -1;
+    Vec3f new_hit_point;
+
+    for (int i = 0; i < scene.GetSize(); ++i) {
+        new_hit_point = scene[i]->CheckHit(*this);
+        if (norm(new_hit_point) < norm(old_hit_point)) {
+            old_hit_point = new_hit_point;
+            hit_obj_idx = i;
+        };
+    };
+
+    if (hit_obj_idx != -1)
+        resCol = scene[hit_obj_idx]->Hit(*this);
+    
+    return resCol;
 };
 
 #endif //__grObj_cpp__
--- graphicObjects.hpp
+++ graphicObjects.hpp
@@ -13,7 +13,8 @@ typedef Vec<3,uint8_t> Color;
 typedef Vec<3,float> Vec3f;
 
 class Scene;
-class GraphObject; 
+class GraphObject;
+class Ray;
 
 class Camera {
     Repere cam_base; //e1 - fwd, e2 - lft, e3 - top
@@ -25,19 +26,25 @@ class Camera {
     Repere mat_base; //origin, e1 = x_base to shift one pixel to the positive x direction in matrix, e2 = y_base --||--.
     Vec2f pixel_size; //cm
 
+    static float render_distance;
+
 public:
     Camera(Vec3f f_origin, Vec3f view_dir, double f_fov = PI/2, Vec2un resolution = Vec2un(1024, 1024),
                                                             int a_channels = 3, Vec2f f_mat_size = Vec2f(10,10)); //warning: channels!
     const Image& RenderImage(const Scene& scene);
 
+    Vec3f GetPixelLocation(unsigned x, unsigned y) const {return mat_base.orig +  x*mat_base.e1 + y*mat_base.e2;};
+    static float GetRenderDistance() {return render_distance;};
+
     //Vec2un MatrixRes() const {return Vec2un(matrix.Width(), matrix.Height());}; 
 };
 
 class Ray {
-    //fields
+    Vec3f orig;
+    Vec3f direction;
 public:
-    Ray(Vec3f f_orig, Vec3f f_dir);
-    Color Cast();
+    Ray(Vec3f f_orig, Vec3f f_dir) {orig = f_orig; direction = normalize(f_dir); };
+    Color Cast(const Scene& scene) const;
 };
 
 class Scene { //use carefully
@@ -47,7 +54,9 @@ public:
     Scene(unsigned f_size) : size(f_size) {data = new GraphObject*[f_size];}
     ~Scene();//u.c.            
 
-    GraphObject*& operator[](unsigned idx); //u.c.
+    unsigned GetSize() const {return size;};
+    GraphObject*& operator[](unsigned idx) const; //u.c.
+    //create object adder
 };
 
 class GraphObject {
@@ -56,6 +65,9 @@ protected:
 public:
     GraphObject(const Repere& f_loc = Repere()) : location(f_loc) {};
     virtual ~GraphObject() {};
+
+    virtual Vec3f CheckHit(const Ray& ray) const = 0;
+    virtual Color Hit(const Ray& ray) const = 0;
 };
 
 class MonochromeSphere : public GraphObject {
@@ -64,6 +76,9 @@ class MonochromeSphere : public GraphObject {
 public:
     MonochromeSphere(const Repere& f_loc = Repere(), float f_rad = 1, const Color& f_col = Color()) : GraphObject(f_loc), radius(f_rad), color(f_col) {};
     virtual ~MonochromeSphere() {};
+
+    virtual Vec3f CheckHit(const Ray& ray) const;
+    virtual Color Hit(const Ray& ray) const;
 };
 
 /*class Emitter : public GraphObject {
